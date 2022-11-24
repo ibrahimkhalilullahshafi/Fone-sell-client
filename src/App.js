6bf7a1f22d847cf335e3c2f81af20c74e6eb8241
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-11-24 20:18:40 +0600

    basic installation done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </div>
   );
 }
